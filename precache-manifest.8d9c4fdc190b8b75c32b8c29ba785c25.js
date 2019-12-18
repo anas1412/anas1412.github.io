7ba8953a9d1d7ccb951f680df31cb1fb41567744
Sassdesu <anas.bassoumi@gmail.com>
2019-12-18 22:18:34 +0100

    test

---
+++ precache-manifest.8d9c4fdc190b8b75c32b8c29ba785c25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbf80bc1da86d7754acf",
+    "url": "/static/css/main.728c1ff2.chunk.css"
+  },
+  {
+    "revision": "bbf80bc1da86d7754acf",
+    "url": "/static/js/main.bbf80bc1.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9c4bbb7f97ad590e16109710e08b0e4c",
+    "url": "/index.html"
+  }
+];
