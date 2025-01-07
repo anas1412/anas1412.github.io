1ace2bd0456b31a3c201432b557aa8294d91b3ba
anas1412 <anas.bassoumi@gmail.com>
2025-01-07 17:11:52 +0100

    added microblogging

---
+++ posts.js
@@ -0,0 +1,81 @@
+window.posts = [
+  {
+    date: "2024-12-10",
+    content: "My Git commits are so clean. 🧹✨",
+  },
+  {
+    date: "2024-12-05",
+    content:
+      "Built a workflow automation tool. Now my to-do list completes itself. 🤖✅",
+  },
+  {
+    date: "2024-10-05",
+    content:
+      "My Python script crashed. Error message: 'You’re trying too hard. Take a break.' 🐍😴",
+  },
+  {
+    date: "2023-11-10",
+    content:
+      "Just finished a Laravel project. PHP is like that old friend who’s always there, even if you don’t talk every day. I love Laravel 💻❤️",
+  },
+  {
+    date: "2023-07-01",
+    content:
+      "Spent hours optimizing Grafana dashboards. Now my metrics are so pretty, they belong in an art gallery. 🎨",
+  },
+  {
+    date: "2023-05-10",
+    content:
+      "My Docker containers are so lightweight, they float. 🐳 Now if only I could say the same about my life.",
+  },
+  {
+    date: "2023-03-15",
+    content:
+      "Terraformed my entire infrastructure today. Now my cloud looks like a Zen garden. 🌿☁️",
+  },
+  {
+    date: "2023-01-20",
+    content: "Accidentally deployed a typo to production. 😅",
+  },
+  {
+    date: "2022-08-05",
+    content:
+      "Built a GitOps pipeline so smooth, it made my GF jealous. 🐱 Now she wants her own ArgoCD setup.",
+  },
+  {
+    date: "2022-07-10",
+    content:
+      "Spent the day debugging Kubernetes pods. Turns out, they’re just like toddlers—cute but unpredictable. I don't like them 🍼",
+  },
+
+  {
+    date: "2022-06-15",
+    content:
+      "Just automated my morning coffee routine with a CI/CD pipeline. ☕️ Now my coffee deploys faster than my code! 🚀",
+  },
+  {
+    date: "2022-05-10",
+    content:
+      "My Kubernetes cluster is running so smoothly, it’s basically a well-oiled machine. ⚙️🛢️",
+  },
+  {
+    date: "2022-05-05",
+    content:
+      "Just got my AWS Cloud Practitioner cert. Now I’m officially a cloud wizard. ☁️🧙‍♂️",
+  },
+  {
+    date: "2022-04-15",
+    content:
+      "Tried to debug a Bash script. It’s like arguing with a toddler who only speaks in riddles. 🐚🤯",
+  },
+  {
+    date: "2022-02-20",
+    content:
+      "Set up Slack alerts for my ELK stack. Now my team gets notified every time I cry over logs. 😭📊",
+  },
+  {
+    date: "2021-09-15",
+    content:
+      "Tried to explain CI/CD to my grandma. She said, 'So it’s like baking a cake, but faster?' 🎂👵",
+  },
+];
