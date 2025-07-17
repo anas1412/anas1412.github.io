9a12276b04ed9f5613882d9813d468a82ba125e9
anas1412 <anas.bassoumi@gmail.com>
2025-07-17 20:24:51 +0100

    added about me as a variable

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,4 +1,5 @@
 window.headerData = {
   title: "Anas B. Cyberspace",
   subtitle: "Portfolio Manager | Quant Trader | Ex-Software Engineer",
+  aboutMe: "I'm Anas, a portfolio manager & quant trader based in Tunisia. Self-educated in finance, business and psychology with a strong educational background in software engineering. I am passionate about spending half of my life making money and spending the other half giving it away."
 };
